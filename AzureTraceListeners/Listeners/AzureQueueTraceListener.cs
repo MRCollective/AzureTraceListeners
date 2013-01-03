8b6b07ead1fe814d4520d2417384f368270189b5
Matt Davies <matthew.davies@curtin.edu.au>
2013-01-03 15:24:56 +0800

    Adding Queue trace listener

---
+++ AzureTraceListeners/Listeners/AzureQueueTraceListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Script.Serialization;
+using AzureTraceListeners.Listeners.Base;
+using AzureTraceListeners.Models;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace AzureTraceListeners.Listeners
+{
+    public class AzureQueueTraceListener : AzureTraceListener
+    {
+        private readonly CloudQueue _queue;
+
+        public AzureQueueTraceListener(string applicationName, string queueConnectionString, string queueName = "tracelogs")
+        {
+            ApplicationName = applicationName;
+            var storageAccount = CloudStorageAccount.Parse(queueConnectionString);
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            _queue = queueClient.GetQueueReference(queueName);
+            _queue.CreateIfNotExist();
+        }
+
+        protected override void SaveMessage(AzureTraceMessage azureTraceMessage)
+        {
+            var serializedMessage = new JavaScriptSerializer().Serialize(azureTraceMessage);
+            _queue.AddMessage(new CloudQueueMessage(serializedMessage));
+        }
+    }
+}
